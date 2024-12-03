593db2a4c6930d922ca028edb0ccfa116e7b515c
Jose060502 <jseda442@gmail.com>
2024-12-03 20:41:13 +0100

    #in progress

--- app/src/main/java/com/example/pokemonhub/MainActivity.kt
+++ app/src/main/java/com/example/pokemonhub/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pokemonhub
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -8,16 +9,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pokemonhub.model.Datasource
+import com.example.pokemonhub.model.Pokemon
 import com.example.pokemonhub.ui.screens.AboutScreen
+import com.example.pokemonhub.ui.screens.PokemonListCompactScreen
+import com.example.pokemonhub.ui.screens.PokemonListMedExpScreen
 import com.example.pokemonhub.ui.theme.PokemonHubTheme
+import com.example.pokemonhub.utils.getWindowSizeClass
 import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
@@ -25,8 +33,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PokemonHubTheme {
-                MainScreen()
+                PokemonApp()
             }
+        }
+    }
+
+    @Composable
+    fun PokemonApp(){
+        val pokemon = Datasource.getListXtimes(1)
+        val windowsize = getWindowSizeClass(LocalContext.current as Activity)
+
+        PokemonHubTheme {
+            Scaffold (
+                modifier = Modifier.fillMaxSize()
+            ) { innerPadding ->
+                when(windowsize){
+                    WindowWidthSizeClass.Compact -> { PokemonListCompactScreen(pokemon, Modifier.padding(innerPadding)) }
+                    else -> { PokemonListMedExpScreen(pokemon, Modifier.padding(innerPadding)) }
+                }
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun MarvelHeroesAppPreview() {
+        PokemonHubTheme  {
+            PokemonApp()
         }
     }
 
@@ -57,6 +90,4 @@ class MainActivity : ComponentActivity() {
             onTimeout()
         }
     }
-
-
 }
--- app/src/main/java/com/example/pokemonhub/ui/components/cards.kt
+++ app/src/main/java/com/example/pokemonhub/ui/components/cards.kt
@@ -80,3 +80,66 @@ fun PokemonCard(pokemon: Pokemon){
         }
     }
 }
+
+@Composable
+fun PokemonLandCard(pokemon: Pokemon){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            IconButton(
+                onClick = { /* Acción futura, como mostrar más información del héroe */ },
+                modifier = Modifier.size(48.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.TwoTone.Favorite,
+                    //painter = painterResource(R.drawable.remove_24px), //Icono descargado de Icons de Material.
+                    modifier = Modifier.size(48.dp),
+                    contentDescription = stringResource(R.string.more_content_desc),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 16.dp)
+            ) {
+                StandardTextComp(
+                    text = pokemon.name,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                StandardTextComp(
+                    text = "Tipo: ${pokemon.type}",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                StandardTextComp(
+                    text = "Habilidad: ${pokemon.ability}",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                StandardTextComp(
+                    text = stringResource(R.string.pokemon_description, pokemon.description),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+
+            }
+
+            ImageComp(
+                modifier = Modifier,
+                drawable = Datasource.getDrawableIdByName(pokemon.photo),
+                height = 100,
+                width = 100
+            )
+        }
+    }
+}
