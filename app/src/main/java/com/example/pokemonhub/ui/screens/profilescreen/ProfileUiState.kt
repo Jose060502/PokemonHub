4a387b1b6d0d426272627be6dff0d5a7d2d5897d
Jose Seda <jseda442@gmail.com>
2025-03-20 14:23:21 +0100

    Update ProfileUiState.kt

--- app/src/main/java/com/example/pokemonhub/ui/screens/profilescreen/ProfileUiState.kt
+++ app/src/main/java/com/example/pokemonhub/ui/screens/profilescreen/ProfileUiState.kt
@@ -5,7 +5,7 @@ import com.example.pokemonhub.data.ModoVisualizacion
 // Representa el estado de la UI para la pantalla de perfil
 data class ProfileUiState(
     val nombreUsuario: String = "", // Nombre del usuario, vacío por defecto
-    val modoVisualizacion: ModoVisualizacion = ModoVisualizacion.SYSTEM, // Modo de visualización del tema (Ej: Claro, Oscuro, Sistema)
+    val modoVisualizacionApp: ModoVisualizacion = ModoVisualizacion.SYSTEM, // Modo de visualización del tema (Ej: Claro, Oscuro, Sistema)
     val userMessage: UserMessage? = null // Mensaje de error opcional, usado para manejar errores en la UI
 )
 
